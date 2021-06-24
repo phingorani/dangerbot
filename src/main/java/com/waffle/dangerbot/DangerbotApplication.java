a9c36ec798e8274ef767b1559c94eb9fe55b969d
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 09:51:05 -0500

    add base controller

--- src/main/java/com/waffle/dangerbot/DangerbotApplication.java
+++ src/main/java/com/waffle/dangerbot/DangerbotApplication.java
@@ -5,15 +5,20 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DangerbotApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DangerbotApplication.class, args);
 
         // Insert your bot's token here
-        String token = System.getenv("DISCORD_TOKEN");
+//        String token = System.getenv("DISCORD_TOKEN");
+        String token = "NDYzMzk5NTgxNzEyMTIxODU4.WzpkfQ.D11Gh5efHBboeRVwOjmUrclAh3U";
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
