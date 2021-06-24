53892edabaf2b9831e0b0e5aa4c59da6da7e5468
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 12:39:46 -0500

    add logger

--- src/main/java/com/waffle/dangerbot/DangerbotApplication.java
+++ src/main/java/com/waffle/dangerbot/DangerbotApplication.java
@@ -3,12 +3,16 @@ package com.waffle.dangerbot;
 import com.waffle.dangerbot.listners.RollListener;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class DangerbotApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(DangerbotApplication.class);
 
     public static void main(String[] args) {
 
