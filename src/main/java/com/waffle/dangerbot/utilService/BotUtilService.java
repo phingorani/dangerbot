6885395f8a8db0703334749c18d5e00fd298c053
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 14:07:29 -0500

    try to fix gradle

--- src/main/java/com/waffle/dangerbot/utilService/BotUtilService.java
+++ src/main/java/com/waffle/dangerbot/utilService/BotUtilService.java
@@ -6,7 +6,7 @@ import org.javacord.api.event.message.MessageCreateEvent;
 public class BotUtilService {
 
     public static Boolean validateBotRollCommand(MessageCreateEvent event, String command) {
-        if (event.getMessageContent().startsWith("!"+command)) {
+        if (event.getMessageContent().startsWith("!"+command) && !event.getMessageAuthor().getDisplayName().equals("DangerBot")) {
             return Boolean.TRUE;
         } else {
             return Boolean.FALSE;
