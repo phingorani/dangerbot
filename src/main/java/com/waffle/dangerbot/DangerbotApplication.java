ea32af76af30587514252039601276a2120bb62d
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-23 19:32:05 -0500

    init

---
+++ src/main/java/com/waffle/dangerbot/DangerbotApplication.java
@@ -0,0 +1,13 @@
+package com.waffle.dangerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangerbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangerbotApplication.class, args);
+    }
+
+}
