de27a7f18b80b937bb2396816a02e12725da447a
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 12:04:16 -0500

    fix controller

--- src/main/java/com/waffle/dangerbot/contollers/IndexController.java
+++ src/main/java/com/waffle/dangerbot/contollers/IndexController.java
@@ -1,13 +1,14 @@
 package com.waffle.dangerbot.contollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
