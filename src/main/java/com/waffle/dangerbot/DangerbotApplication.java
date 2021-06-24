fd306d720e7d166f3ac95768d1aa334bae380247
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 15:49:36 -0500

    fix web servlet init

--- src/main/java/com/waffle/dangerbot/DangerbotApplication.java
+++ src/main/java/com/waffle/dangerbot/DangerbotApplication.java
@@ -7,10 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class DangerbotApplication {
+public class DangerbotApplication extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(DangerbotApplication.class);
 
