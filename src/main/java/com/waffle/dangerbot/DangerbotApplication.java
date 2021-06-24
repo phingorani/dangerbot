b90719672650879735cfa8beba4b35a8dec8ee0b
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 10:24:36 -0500

    add manifest

--- src/main/java/com/waffle/dangerbot/DangerbotApplication.java
+++ src/main/java/com/waffle/dangerbot/DangerbotApplication.java
@@ -5,9 +5,7 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @SpringBootApplication
 public class DangerbotApplication {
