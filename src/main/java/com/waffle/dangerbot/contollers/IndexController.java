e46c5de2d74bd9383a1f32c77df0177f5f467dfc
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-06-24 15:59:33 -0500

    add tomcat

--- src/main/java/com/waffle/dangerbot/contollers/IndexController.java
+++ src/main/java/com/waffle/dangerbot/contollers/IndexController.java
@@ -1,9 +1,7 @@
 package com.waffle.dangerbot.contollers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
