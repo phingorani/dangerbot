4f27cd2dc39b3f0a7a2aa74e811a609a7010cde8
phingorani <hingorani.pratik@gmail.com>
2021-06-29 23:08:17 -0500

    change db

--- src/main/java/com/waffle/dangerbot/entity/User.java
+++ src/main/java/com/waffle/dangerbot/entity/User.java
@@ -5,11 +5,38 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user")
 public class User {
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Long getDiscordId() {
+        return discordId;
+    }
+
+    public void setDiscordId(Long discordId) {
+        this.discordId = discordId;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @Column(name = "display_name")
     private String displayName;
+
+    @Column(name = "discord_id")
     private Long discordId;
 
     public User(Long userId, String displayName, Long discordId) {
