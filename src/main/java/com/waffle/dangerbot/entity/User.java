796e32eb7b5e28b8f82c3951421a7963141dd953
phingorani <hingorani.pratik@gmail.com>
2021-06-30 01:55:30 -0500

    column fix

--- src/main/java/com/waffle/dangerbot/entity/User.java
+++ src/main/java/com/waffle/dangerbot/entity/User.java
@@ -31,6 +31,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @Column(name = "display_name")
