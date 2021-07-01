ed85ecedd227f92f772acbea9550177764d2a042
Pratik Hingorani <hingorani.pratik@gmail.com>
2021-07-01 09:31:24 -0500

    get guild members

--- src/main/java/com/waffle/dangerbot/listeners/UpdateUsersListener.java
+++ src/main/java/com/waffle/dangerbot/listeners/UpdateUsersListener.java
@@ -3,11 +3,14 @@ package com.waffle.dangerbot.listeners;
 import com.waffle.dangerbot.entity.DiscordUser;
 import com.waffle.dangerbot.repository.DiscordUserRepository;
 import com.waffle.dangerbot.utilService.BotUtilService;
-import org.javacord.api.entity.permission.Role;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class UpdateUsersListener implements MessageCreateListener {
@@ -17,6 +20,28 @@ public class UpdateUsersListener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String baseUrl = System.getenv("DISCORD_API_URL");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("authorization", event.getApi().getToken());
+        headers.set("Accept", "application/json");
+        HttpEntity entity = new HttpEntity(headers);
+
+        String fooResourceUrl
+                = baseUrl+"/guilds/"+event.getServer().get().getId()+"/members";
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl);
+        builder.queryParam("limit", 1000);
+
+
+        ResponseEntity<?> response
+                = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity,User.class);
+
+        System.out.println(response.getBody());
+
         if (BotUtilService.isValidateBotCommand(event)) {
             if (BotUtilService.isAdmin(event) && BotUtilService.isUpdateUsers(event)) {
                 event.getServer().get().getRoles().forEach(role -> System.out.println(role.getName()));
